a67af19789d1482b09d94ac130f0b4d713cd940e
HastingsJack <joha0005@stud.kea.dk>
2025-09-18 10:10:17 +0200

    azure friendly test

--- src/test/java/org/example/jpastudente24a/repositories/StudentRepositoryTest.java
+++ src/test/java/org/example/jpastudente24a/repositories/StudentRepositoryTest.java
@@ -1,24 +1,32 @@
 package org.example.jpastudente24a.repositories;
 
 import org.example.jpastudente24a.model.Student;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     StudentRepository studentRepository;
 
+    @BeforeEach
+    void setUp() {
+        Student student = new Student();
+        student.setName("test");
+        studentRepository.save(student);
+    }
     @Test
     void testOneStudent() {
         List<Student> list = studentRepository.findAll();
-        assertEquals(2, list.size());
+        assertEquals(1, list.size());
     }
 
 }
